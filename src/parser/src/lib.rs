c0835cd0c8f9cf1db5eed5034c65dc317a1b9942
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2022-02-20 22:44:42 +0800

    add is token type

--- src/parser/src/lib.rs
+++ src/parser/src/lib.rs
@@ -1,13 +1,50 @@
-use util::token::Token;
-
 mod test;
 
-pub struct Parser {
-    pub tokens: Vec<Token>
+use util::token::Token;
+use util::token::Tokens;
+use std::slice::Iter;
+
+pub struct Parser<'a> {
+    pub tokens: Iter<'a, Token>,
+    pub index: usize,
+    currently: &'a Token,
+    is_eof: bool,
 }
 
-impl Parser {
-    pub fn parse(&self) {
+impl Parser<'_> {
+    pub fn new(tokens: &Vec<Token>) -> Parser {
+        let mut tokens_iter = tokens.iter();
+        let next_token = tokens_iter.next().unwrap();
 
+        Parser {
+            tokens: tokens_iter,
+            index: 0,
+            is_eof: next_token.is_token_type(Tokens::EOF),
+            currently: next_token,
+        }
+    }
+
+    pub fn next_token(&mut self) {
+        let token = self.tokens.next();
+        self.is_eof = token.is_none();
+
+        if !self.is_eof {
+            if token.unwrap().is_token_type(Tokens::EOF) {
+                self.currently = token.unwrap();
+                self.index += 1;
+            }
+        }
+    }
+
+    pub fn parse(&mut self) {
+        while !self.is_eof {
+            if self.currently.is_token_type_str("LiteralToken") {
+                if let Tokens::LiteralToken { kid, literal } = &self.currently.token_type {
+                    println!("{:?}: {}", kid, literal);
+                };
+
+            }
+            self.next_token();
+        }
     }
 }
--- src/parser/src/test/test_parser.rs
+++ src/parser/src/test/test_parser.rs
@@ -1,7 +1,29 @@
 #[cfg(test)]
 mod test_parser {
+    use crate::Parser;
+    use util::token::{Token, Position, Tokens, Literal};
+
     #[test]
     fn test_parser() {
+        let pos = Position {
+            start: 0,
+            end: 0,
+        };
 
+        let tokens = vec![
+            Token {
+                token_type: Tokens::LiteralToken {
+                    kid: Literal::String,
+                    literal: "abc".to_string(),
+                },
+                pos: pos.clone(),
+            },
+            Token {
+                token_type: Tokens::EOF,
+                pos: pos.clone(),
+            },
+        ];
+        let mut parser = Parser::new(&tokens);
+        parser.parse();
     }
 }
