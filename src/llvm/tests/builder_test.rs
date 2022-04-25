76f770e17bcd831793e7812f7043f0df3a7605a3
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2022-04-25 22:19:55 +0800

    llvm: fix test

--- src/llvm/tests/builder_test.rs
+++ src/llvm/tests/builder_test.rs
@@ -1,58 +1,7 @@
 use llvm::builder::LLVMBuilder;
-use llvm::context::{GlobalVariableContext, LLVMContext};
 use llvm::linkage_types::LinkageTypes;
 use llvm::llvm_type::LLVMTypes;
-use llvm::value::{create_number, create_ref_string, Value, ValueType};
-
-#[test]
-fn global_variable_context() {
-    let str = GlobalVariableContext {
-        linkage: LinkageTypes::NULL,
-        is_constant: false,
-        variable_name: "a".to_string(),
-        value: Value {
-            context: "2".to_string(),
-            value_type: ValueType::Other,
-        },
-        value_type: LLVMTypes::Int8,
-    }
-        .to_string();
-
-    let expect_test = r##"@a = dso_local global i8 2, align 1"##;
-
-    assert_eq!(expect_test, &str);
-}
-
-#[test]
-fn context_test() {
-    let global_variables = vec![GlobalVariableContext {
-        linkage: LinkageTypes::NULL,
-        is_constant: false,
-        variable_name: "a".to_string(),
-        value: Value {
-            context: "12".to_string(),
-            value_type: ValueType::Other,
-        },
-        value_type: LLVMTypes::Int8,
-    }];
-
-    let source_filename = "test.zx".to_string();
-
-    let llvm_ir = LLVMContext {
-        source_filename,
-        global_variables,
-        named_metadata: vec![],
-    }
-        .to_string();
-    let expect_test = r##"; ModuleID = 'test.zx'
-source_filename = "test.zx"
-
-@a = dso_local global i8 12, align 1
-
-
-"##;
-    assert_eq!(expect_test, &llvm_ir);
-}
+use llvm::value::{create_number, create_ref_string};
 
 #[test]
 fn builder_global_var_string_test() {
@@ -114,4 +63,3 @@ source_filename = "test.zx"
 "##;
     assert_eq!(expect_test, &llvm_ir);
 }
-
